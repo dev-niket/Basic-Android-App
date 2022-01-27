386c6ce2310792f92409bda03eae63a6e60b7780
dev-niket <niketganatra06@gmail.com>
2022-01-27 01:54:58 -0800

    Invoking intents to other applications

--- app/src/main/java/com/example/firstapp/LoginActivity.kt
+++ app/src/main/java/com/example/firstapp/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.example.firstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
@@ -15,6 +17,38 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun clickHandler(view: View) {
-        Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+        when(view.id){
+
+            R.id.btnLogin -> {
+                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.btnAlarm -> {
+                /*
+                NOTE:
+                This is just a sample intent to another app and does not really have any sense in the context of the app.
+                This is just for beginners to understand how can we invoke intents to other applications.
+                You can find more about common intents used in Android at the following link
+                https://developer.android.com/guide/components/intents-common
+                */
+                /*
+                NOTE 2:
+                Make sure that you have added the permission line in the AndroidManifest.xml file
+                You can refer the Commit with the message 'Invoking intents to other applications' in order to understand where the permission line needs to be added and what exactly it is.
+                You can also find the permission line on the above link which gives the information of all common intents
+                 */
+
+
+                val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+                    putExtra(AlarmClock.EXTRA_MESSAGE, "Sample Alarm")
+                    putExtra(AlarmClock.EXTRA_HOUR, 12)
+                    putExtra(AlarmClock.EXTRA_MINUTES, 0)
+                }
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.firstapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,6 +13,31 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginClickHandler(view: View) {
+        when(view.id){
+            R.id.btnLoginPage -> {
+                startLoginActivity()
+            }
+            R.id.btnDial -> {
+                startDialer()
+            }
+        }
+
+    }
+
+    private fun startDialer() {
+        /*
+        NOTE:
+        This is just a sample intent to another app and does not really have any sense in the context of the app.
+        This is just for beginners to understand how can we invoke intents to other applications.
+        You can find more about common intents used in Android at the following link
+                https://developer.android.com/guide/components/intents-common
+         */
+
+        val dialIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:12345678"))
+        startActivity(dialIntent)
+    }
+
+    private fun startLoginActivity() {
         var intent: Intent
         intent = Intent(this, LoginActivity::class.java)
         intent.putExtra("check", "Checking Data Transfer")
