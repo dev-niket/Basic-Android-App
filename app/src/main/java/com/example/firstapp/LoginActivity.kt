77833ced690532b17026b56208e70a983df29bec
dev-niket <niketganatra06@gmail.com>
2022-01-26 23:35:03 -0800

    Passing data from one activity to another.

--- app/src/main/java/com/example/firstapp/LoginActivity.kt
+++ app/src/main/java/com/example/firstapp/LoginActivity.kt
@@ -3,12 +3,15 @@ package com.example.firstapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        var tvLogin = findViewById<TextView>(R.id.textView)
+        tvLogin.text = intent.getStringExtra("check")
     }
 
     fun clickHandler(view: View) {
--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     fun loginClickHandler(view: View) {
         var intent: Intent
         intent = Intent(this, LoginActivity::class.java)
+        intent.putExtra("check", "Checking Data Transfer")
         startActivity(intent)
     }
 }
